818ae623247d3bcb8bc6de83ec50784fa43b5b01
RandimaD25 <randimamethminid525@gmail.com>
2025-09-16 17:12:48 +0530

    feat: Dependency Injection - through a setter

--- store/src/main/java/com/codewithrandima/store/StoreApplication.java
+++ store/src/main/java/com/codewithrandima/store/StoreApplication.java
@@ -11,11 +11,16 @@ public class StoreApplication {
 //		SpringApplication.run(StoreApplication.class, args);
 
 //		inject the (new StripePaymentService) which is an object through OrderService constructor
-		var orderService1 = new OrderService(new StripePaymentService());
-		orderService1.placeOrder();
+//		var orderService1 = new OrderService(new StripePaymentService());
+//		orderService1.placeOrder();
+//
+//		var orderService2 = new OrderService(new PaypalPaymentService());
+//		orderService2.placeOrder();
 
-		var orderService2 = new OrderService(new PaypalPaymentService());
-		orderService2.placeOrder();
+//		Inject through a setter (Setter injection)
+		var orderService3 = new OrderService();
+		orderService3.setPaymentService(new StripePaymentService());
+		orderService3.placeOrder();
 	}
 
 }
